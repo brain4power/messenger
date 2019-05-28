bd5fa1cedc1dcff9e89bc6643af36224fb948430
brain4power <yotakrasnodar@yandex.ru>
2019-05-28 14:57:49 +0300

    добавил инициализацию сессии и пару классовых методов - получение всех User и по лучение контактов пользователя

--- server/database/database.py
+++ server/database/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from datetime import datetime
 
@@ -10,32 +11,40 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    login = Column(String)
+    login = Column(String, unique=True)
     information = Column(String)
 
     def __init__(self, login, information=''):
         self.login = login
         self.information = information
 
+    @classmethod
+    def get_all_users(cls, session):
+        return session.query(cls).all()
+
+    def __repr__(self):
+        return '{}'.format(self.login)
+
 
 class UserContact(Base):
     __tablename__ = 'user_contact'
     id = Column(Integer, primary_key=True)
-    owner_id = Column(Integer, ForeignKey(User.__tablename__.join('.id')))
-    contact_id = Column(Integer, ForeignKey(User.__tablename__.join('.id')))
+    owner_id = Column(Integer, ForeignKey('user.id'))
+    contact_id = Column(Integer, ForeignKey('user.id'))
 
     def __init__(self, owner_id, contact_id):
         self.owner_id = owner_id
         self.contact_id = contact_id
 
-    def get_user_contacts(self, session, owner_id):
-        pass
+    @classmethod
+    def get_user_contacts(cls, session, owner_id):
+        return session.query(cls.contact_id).filter(cls.owner_id == owner_id).one()
 
 
 class UserHistory(Base):
     __tablename__ = 'user_history'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.__tablename__.join('.id')))
+    user_id = Column(Integer, ForeignKey('user.id'))
     enter_time = Column(Integer)
     ip_address = Column(String)
 
@@ -43,3 +52,8 @@ class UserHistory(Base):
         self.user_id = user_id
         self.ip_address = ip_adress
         self.enter_time = datetime.now()
+
+
+Base.metadata.create_all(engine, checkfirst=True)
+
+Session = sessionmaker(bind=engine)
